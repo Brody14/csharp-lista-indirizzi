2dba9af1f835e396df69fbd2f707bee67e913145
Brody14 <stefania.spinas@gmail.com>
2023-09-14 16:46:28 +0200

    Esercizio completato

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Street {  get; set; }
+        public string City { get; set; }
+
+        public string Province { get; set; }
+        public int ZipCode { get; set; }
+
+        public Address(string firstName, string lastName, string street, string city, string province, int zipCode) 
+        { 
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"L'indirizzo di {this.FirstName} {this.LastName} è: {this.Street}, {this.City}, {this.Province}, {this.ZipCode} ";
+        }
+    }
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Address> addresses = new List<Address>();
+
+            try
+            {
+                StreamReader fileAddresses = File.OpenText("C:\\Users\\stefa\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\my-addresses.csv");
+
+                int counter = 0;
+
+                while (!fileAddresses.EndOfStream)
+                {
+                    string line = fileAddresses.ReadLine();
+                    counter++;
+
+                    if (counter > 1)
+                    {
+                        string[] addressPieces = line.Split(',');
+
+                        if (addressPieces.Length < 6)
+                        {
+                            Console.WriteLine($"{line} non è in un formato corretto!");
+                        }
+                        else
+                        {
+                            string firstName = addressPieces[0];
+                            string lastName = addressPieces[1];
+                            string steet = addressPieces[2];
+                            string city = addressPieces[3];
+                            string province = addressPieces[4];
+                            int zipCode = int.Parse(addressPieces[5]);
+
+                            Address newAddress = new Address(firstName, lastName, steet, city, province, zipCode);
+                            Console.WriteLine("letto");
+
+                            addresses.Add(newAddress);
+                        }
+                    }
+                }
+
+
+                fileAddresses.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            foreach(Address add in addresses)
+            {
+                Console.WriteLine(add);
+            }
         }
     }
 }
